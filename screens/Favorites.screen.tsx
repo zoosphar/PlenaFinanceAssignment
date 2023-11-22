81dd7441dc15519269c99c0752202c3a2ef74fdf
aviralsingh-lpl <aviral.singh@littleplace.com>
2023-11-22 15:22:39 +0530

    basic setup with routing

---
+++ screens/Favorites.screen.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+export default function Favorites(): JSX.Element {
+  return (
+    <View>
+      <Text>Favorites</Text>
+    </View>
+  );
+}
---
+++ screens/Home.screen.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export default function Home(): JSX.Element {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
